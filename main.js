b295733d40055ecc52737deee790001b243ef370
Daniil <daniilminin1990@yandex.ru>
2023-07-19 14:01:51 +0600

    12-5 отображение формы

--- main.js
+++ main.js
@@ -40,67 +40,91 @@ Geolocation.watchPosition() (en-US): зарегистрирует функцию
 
 Мы же напишем прям сразу navigator.geolocation.getCurrentPosition();
 */
+let map;
+let mapEvent;
+
 if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const {
-        coords: { latitude },
-      } = position;
-      const { longitude } = position.coords;
-      const coords = [latitude, longitude];
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const {
+      coords: { latitude },
+    } = position;
+    const { longitude } = position.coords;
+    const coords = [latitude, longitude];
 
-      const map = L.map("map").setView(coords, 13);
+    map = L.map("map").setView(coords, 13);
 
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
-      console.log(map);
-      map.on("click", function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: "mark-popup",
-            })
-          )
-          .setPopupContent("Тренировка")
-          .openPopup();
-      });
-    },
-    function () {
-      alert("Вы не предоставили доступ к своей локации");
-    }
-  );
+    console.log(map);
+    map.on(
+      "click",
+      function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+      },
+      function () {
+        alert("Вы не предоставили доступ к своей локации");
+      }
+    );
+
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      inputDistance.value =
+        inputDuration.value =
+        inputElevation.value =
+        inputCadence.value =
+          "";
+      console.log(mapEvent);
+      const { lat, lng } = mapEvent.latlng;
+      L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: "mark-popup",
+          })
+        )
+        .setPopupContent("Тренировка")
+        .openPopup();
+    });
+  });
+inputType.addEventListener("change", function (e) {
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+});
 
 /* 
-  todo 12-4 Работаем с документацией
-  Поработаем с документацией, научимся ставить маркеры когда нажимаем на карту, кастомизируем popUpы
+todo 12-5 Отображение формы
+ui form это родной html тег у него класс hidden
+Будем отображать, чтобы можно было вводить значения
+Впишем это в map.on("click") перед определением координат,
+а саму реализацию появления маркера по клику нужно будет потом переместить в другое место, т.к. маркер будет появляться когда будем отправлять форму
+Сделаем курсор на InputDistance, когда жмякнем на карту (т.е. в map.on(click))
 
-  Работать будем не с addEventListener, а с методами Leaflet
-  Выведем в консоль map
-  Развернем Prototype, потом еще один и там есть метод "on"
-  Будем работать с ним (это из документации)
-  L.marker поместим в map.on() / Зачем? потому что маркер мы хотим получить по действию. 
-  Метод on находится не в самом map, а в его родителе, то есть с помощью протоипного наследования мы можем обращаться к его методам
-  map.on принимает несколько параметров:
-  click
-  function(mapEvent)
-  у mapEvent есть свои свойства, они то нам и понадобятся, там лежат координаты точки, куда нажали на карту. Мы передадим эти координаты в L.marker
-  
+Для form тоже сделаем addEventListener по "submit" и сюда вставим L.marker
 
-  Теперь работает маркер по нажатию
+Но у нас остались проблемы - переменная map не глобальная, она создана в методе getCurrentPosition, соответственно ограничивается этой областью видимости и нам нужно эту переменную сделать глобальной. Вынесем ее за скобки функции
 
-  Теперь нужно настроить popup.
-  Можно настроить через CSS (НО ЭТО МОЖЕТ НАРУШИТЬ СТАНДАРТНУЮ СТИЛИСТИКУ), а можно через встроенные методы, используя документацию API
-  Мы будем делать через css
+Другая проблема - у нас нет mapEvent, которая в map.on function (mapEvent). Сделаем ее глобальной тоже
 
+У нас нет кнопки для отправки формы. Но по умолчанию при нажатии  ина Enter, форма отправляется
 
-  */
+Отменяем стандартное поведение для form.addEventListener, чтобы страница не перезагружалась после отправки формы
+И теперь все реализовано. При клике на точку на карте появляется форма, и при отправке формы, появляется маркер
+
+Очищаем input после отправки. inputDistance = "";
+
+У нас бег связан с высотой, а велосипед с темпом. Выбор велосипеда или бега - inputType
+В HTML есть высота, скрытая hidden. И нужно ее проявлять, а темп удалять, когда меняем тип
+Создадим изменение для inputType и воспользоваться событием change
+Так как inputCadence и inputElevation определены как document.querySelector(".form__input--cadence"), а класс form__row--hidden находится в div form__row, который объединяет label и этот input, то нам нужно менять classList содержащий hidden, то есть div, а как это сделать, не определяя этот div?! Воспользуемся методом closest().
+Этот метод возвращает ближайший родительский элемент, который соответствует заданному CSS селектору
+Чтобы так сделать, нужно дописать в inputCadence.closest(".form__row").classList.toggle("....") 
+*/
